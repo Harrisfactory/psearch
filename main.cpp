1c7fe820521c31e1ed26beaa829404184c5d3a78
Harris Schrick <schrickh+github@gmail.com>
2024-02-14 16:42:13 -0800

    semi works with simple linear search

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@
 // argv is the arguments themselves
 int main (int argc, char* argv[]) {
     // I just want the first argument for the path
-    if (argc != 2) {
-        std::cout << "This command only accepts a file path \n";
+    if (argc != 3) {
+        std::cout << "This command only accepts a pattern & a file path as arguments \n";
         return -1;
     }
 
@@ -18,12 +18,23 @@ int main (int argc, char* argv[]) {
     std::string psearchResult;
 
     // read from users file
-    std::ifstream userFile(argv[1]);
+    std::ifstream userFile(argv[2]);
 
-    // currently outputting every line for debug purposes
+    // Implementing a simple linear search for now.
+    int patternLen = strlen(argv[1]);
+    char patternFirstChar = argv[1][0];
+    int lineCtr = 0;
     while (getline (userFile, psearchResult)) {
-        // Output the text from the file
-        std::cout << psearchResult;
+        lineCtr++;
+        //cycle through current line looking for the first character of the pattern
+        for (int i = 0; i < psearchResult.length(); i++) {
+            if (psearchResult[i] == patternFirstChar) {
+                //check substring of pattern len
+                if (psearchResult.substr(i, patternLen) == argv[1]) {
+                    std::cout << "result found on line: " << lineCtr << " line contents: " << psearchResult << "\n";
+                }
+            }
+        }
     }
 
     // closing the file to be memory safe
